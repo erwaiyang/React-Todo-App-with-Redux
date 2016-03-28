3b37ef0e74f8035675fbe1bbc4bf39ea171fe945
Jean Yang <bdlas321@gmail.com>
2016-03-28 23:40:46 +0800

    start style

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
+require ('../stylesheets/app.scss');
+
 import store from './store';
 import AppContainer from './containers';
 
