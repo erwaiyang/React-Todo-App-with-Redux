5dfce2274f956345c581c8f65b8ec8b982d91b18
Jean Yang <bdlas321@gmail.com>
2016-03-29 23:41:23 +0800

    TDD todo reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 
-export default combineReducers({
+import { todoReducer } from './todoReducer';
 
+export default combineReducers({
+  todoReducer
 });
---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,61 @@
+import * as C from '../constants/actionTypes';
+
+export const initialState = [
+  {
+    id: 0,
+    text: 'my todo',
+    active: true,
+    starred: false
+  }
+];
+
+export function todoReducer(state = initialState, action) {
+  switch (action.type) {
+    case C.ADD_TODO:
+      let theBiggestId = -1;
+      for(var i = 0; i<state.length; i++){
+        if(state[i].id>theBiggestId)
+          theBiggestId = state[i].id;
+      }
+      return [
+        ...state,
+        {
+          id: Math.max( state.length, (theBiggestId + 1) ),
+          active: true,
+          starred: false,
+          text: action.text
+        }
+      ];
+
+    case C.DEL_TODO:
+      return state.filter(todo =>
+        todo.id !== action.id
+      );
+
+    case C.EDIT_TODO:
+      return state.map(todo =>
+        todo.id === action.id ?
+          Object.assign({}, todo, { text: action.text }) :
+          todo
+      );
+
+    case C.COMPLETE_TODO:
+    case C.UNCOMPLETE_TODO:
+      return state.map(todo =>
+        todo.id === action.id ?
+          Object.assign({}, todo, { active: !todo.active }) :
+          todo
+      );
+
+    case C.STAR_TODO:
+    case C.UNSTAR_TODO:
+      return state.map(todo =>
+        todo.id === action.id ?
+          Object.assign({}, todo, { starred: !todo.starred }) :
+          todo
+      );
+
+    default:
+      return state;
+  }
+};
