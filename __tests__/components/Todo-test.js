c2c5868b9b2f0da90299faf6594c610a08094c3a
Jean Yang <bdlas321@gmail.com>
2016-04-03 03:27:00 +0800

    writing a testing suite for Todo

---
+++ __tests__/components/Todo-test.js
@@ -0,0 +1,24 @@
+jest.unmock('../../src/components/Todo');
+
+import Todo from '../../src/components/Todo';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+
+describe('components', () => {
+  describe('Todo', () => {
+    it('generates an input when its text is clicked', () => {
+
+    });
+    it('triggers STAR_TODO when star icon is clicked', () => {
+
+    });
+    it('triggers COMPLETE_TODO when check icon is clicked', () => {
+
+    });
+    it('triggers DEL_TODO when trash icon is clicked', () => {
+
+    });
+  });
+});
