e9e66777616238f888b5254ccc56cb80732c4949
Jean Yang <bdlas321@gmail.com>
2016-04-05 13:37:37 +0800

    sticky nav+input area

--- src/containers/index.js
+++ src/containers/index.js
@@ -10,13 +10,39 @@ import TodoList from '../components/TodoList';
 import * as todoActions from '../actions/todoActions'
 
 class AwesomeTodoApp extends Component {
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event){
+    let scrollTop = event.srcElement.body.scrollTop;
+    const stickyInstance = document.getElementById('sticky');
+    const stickyHelperInstance = document.getElementById('sticky-helper');
+    if(stickyInstance.getClientRects()[0].top<0){
+      stickyInstance.className += ' sticky';
+      stickyHelperInstance.style.height = stickyInstance.offsetHeight+'px';
+    }
+    if(scrollTop<(stickyInstance.offsetHeight+stickyInstance.offsetTop)){
+      stickyInstance.className = '';
+      stickyHelperInstance.style.height = 0;
+    }
+  }
+
   render() {
     const { todos, actions } = this.props;
     return (
       <div className="my-app">
         <Logo />
-        <Nav />
-        <InputArea addTodo={actions.addTodo} />
+        <div id="sticky">
+          <Nav />
+          <InputArea ref="input_area" addTodo={actions.addTodo} />
+        </div>
+        <div id="sticky-helper" />
         <TodoList todos={todos} actions={actions} />
       </div>
     );
