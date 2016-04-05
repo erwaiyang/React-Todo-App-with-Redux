a76a8dbb6bf18321bdba48ed992410b9d64a47d7
Jean Yang <bdlas321@gmail.com>
2016-04-06 02:13:21 +0800

    版本1

--- src/constants/actionTypes.js
+++
@@ -1,8 +0,0 @@
-export const ADD_TODO = 'ADD_TODO';
-export const DEL_TODO = 'DEL_TODO';
-export const EDIT_TODO = 'EDIT_TODO';
-
-export const COMPLETE_TODO = 'COMPLETE_TODO';
-export const UNCOMPLETE_TODO = 'UNCOMPLETE_TODO';
-export const STAR_TODO = 'STAR_TODO';
-export const UNSTAR_TODO = 'UNSTAR_TODO';
